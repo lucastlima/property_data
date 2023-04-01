9ac7ced6fb04ed458c3a301e638fcbf4eec14c80
Lucas Lima <lucastlima86@gmail.com>
2023-04-01 19:51:32 +0100

    Update deno

--- postprocess.js
+++ postprocess.js
@@ -1,8 +1,9 @@
 import { parse } from "https://deno.land/std/encoding/csv.ts"
 // First get the csv data
-const rawContent = Deno.readFileSync(Deno.args[0])
+const rawContent = Deno.readFileSync("UK-HPI-full-file-2023-01.csv")
 const decodedContent = new TextDecoder().decode(rawContent)
 // Parse it
 const parsed = await parse(decodedContent)
 // parsed is already valid JSON. which results in a value that is exactly the same that CSV to JSON would produce
-await writeJson("uk_HPI_full_file_2023.json", parsed)
+console.log(parsed);
+//await writeJson("uk_HPI_full_file_2023.json", parsed)
